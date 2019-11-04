930d5e5fe60c448ced7b5393f9e72b1b24cdf823
marciks <marcinhocfa@gmail.com>
2019-11-04 16:55:02 -0300

    Update script

--- debian_install.sh
+++ debian_install.sh
@@ -8,6 +8,8 @@ sudo apt-get -y install curl
 
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
+sudo usermod -a -G docker $(whoami)
+sudo service docker start
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
