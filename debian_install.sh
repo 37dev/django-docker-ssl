450d86a288e90bb80bfa4af7b1b9d076f2cd2231
marciks <marcinhocfa@gmail.com>
2019-11-04 16:51:09 -0300

    Update script

--- debian_install.sh
+++ debian_install.sh
@@ -3,8 +3,8 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 
-sudo apt-get install --reinstall make
-sudo apt-get install curl
+sudo apt-get -y install --reinstall make
+sudo apt-get -y install curl
 
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
@@ -12,7 +12,6 @@ sh get-docker.sh
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-sudo apt-get install haveged
+sudo apt-get -y install haveged
 sudo DAEMON_ARGS="-w 1024"
 sudo update-rc.d haveged defaults
-
